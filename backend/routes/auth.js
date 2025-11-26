cfb03a95eb8b9aafcbeaf9e89e9f4117017a0059
Jaspreet Huda <roshan7677@gmail.com>
2025-11-26 08:07:01 +0530

    Implementing a JWT token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Roshiisagood$boy";
+
 // Create a user using : POST: "/api/auth". Doesn't require Auth. No login reqd.
 router.post(
   "/createuser",
@@ -34,7 +37,17 @@ router.post(
         email: req.body.email,
         password: secPass,
       });
-      res.json({ Nice: "nice" });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      console.log(authtoken);
+      res.json({ authtoken });
+
+      // res.json(user);
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some error occurred");
