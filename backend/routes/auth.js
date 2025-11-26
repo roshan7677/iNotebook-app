ba6cb3b05733bc04ebd860da41a08bf72d57ae3c
Jaspreet Huda <roshan7677@gmail.com>
2025-11-26 07:45:14 +0530

    Implemented hasing password, and genSalt using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require("bcryptjs");
 // Create a user using : POST: "/api/auth". Doesn't require Auth. No login reqd.
 router.post(
   "/createuser",
@@ -25,10 +25,14 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email ID already exists." });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
       res.json({ Nice: "nice" });
     } catch (error) {
